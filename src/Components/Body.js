efc8e7b65f2dfda215ab6e77c0317d3fd4a48e78
Karan Jaiswal <jaiskaran008@gmail.com>
2023-04-05 17:22:17 +0000

    search fixed by me

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,22 +1,32 @@
 import {restaurantList} from "../constants"
 import RestaurantCard from "./RestaurantCard"
 import { useState } from "react"
+
+function filterSearch(restaurants,searchText){
+    return restaurants.filter((restaurant) => restaurant.data.name.toLowerCase().includes(searchText.toLowerCase()));
+}
 export default Body = ()=>{
     const [searchText, setSearchText] = useState("");
+    const [restaurants,setRestaurants] =useState(restaurantList);
     return (
         <>
             <div className="search-container">
                 <input  type="text" className="search-input" 
                 placeholder="search" value={searchText} onChange={(e) => {
                     setSearchText(e.target.value);
+                    const data= filterSearch(restaurantList,searchText);
+                    setRestaurants(data);
                 }}/>
                 
-                <button style={{backgroundColor:"black", color:"white",margin:"5px",height:"30px",borderRadius:"5px"}}
-                 className="search-button">search {searchText}</button>
+                <button style={{backgroundColor:"black", color:"white",margin:"5px", height:"100%" ,borderRadius:"5px"}}
+                 className="search-button" onClick={() =>{
+                    const data= filterSearch(restaurantList,searchText);
+                    setRestaurants(data);
+                 }}>search </button>
             </div>
             <div className="restaurant-list">
            
-                {restaurantList.map( (restaurant) =>{
+                {restaurants.map( (restaurant) =>{
                      return <RestaurantCard key={restaurant.data.id} {...restaurant.data}/>
                  })}
             </div>
